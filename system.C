f8bd5d3bf1cdc0f0217cbe22d28a9200c1b01a2e
unive1017 <tjgh219@gmail.com>
2023-06-01 22:55:03 +0900

    stage.c 파일 추가 stage 관련 내용은 stage.c에 추가할 것 main.c 코드 정리 stage.h 정리. GetStageInfor 함수 .h로 옮김 system.c 추가 내용: 플레이어 키 입력, 충돌 처리, 플레이어 주변 시야 확장 system.h에 system.c 함수들 정의

--- system.C
+++ system.C
@@ -1,26 +1,108 @@
 #include "System.h"
-#include<Windows.h>
 
 COORD GetCurrentCursorPos(void)
 {
-    COORD curPoint;
-    CONSOLE_SCREEN_BUFFER_INFO curInfo;
+	COORD curPoint;
+	CONSOLE_SCREEN_BUFFER_INFO curInfo;
 
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
-    curPoint.X = curInfo.dwCursorPosition.X;
-    curPoint.Y = curInfo.dwCursorPosition.Y;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curPoint.X = curInfo.dwCursorPosition.X;
+	curPoint.Y = curInfo.dwCursorPosition.Y;
 
-    return curPoint;
+	return curPoint;
 }
 void RemoveCursor(void)
 {
-    CONSOLE_CURSOR_INFO curInfo;
-    GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
-    curInfo.bVisible = 0;
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	CONSOLE_CURSOR_INFO curInfo;
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
+	curInfo.bVisible = 0;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
 }
 void SetCurrentCursorPos(int x, int y)
 {
-    COORD pos = { x, y };
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void ProcessKeyInput() {
+	int key;
+
+	if (_kbhit() != 0) {
+		key = _getch();
+		switch (key) {
+		case LEFT:
+			playerMove(LEFT);
+			break;
+		case RIGHT:
+			playerMove(RIGHT);
+			break;
+		case UP:
+			playerMove(UP);
+			break;
+		case DOWN:
+			playerMove(DOWN);
+			break;
+		}
+	}
+}
+
+int DetectCollision(int posX, int posY) {
+	return STAGE[posY][posX / 2];
+}
+void showSoundRange(COORD curPos, int objNum, int range) {
+	int posX = curPos.X / 2;
+	int posY = curPos.Y;
+
+	for (posX = curPos.X / 2; posX >= 0 && posX > curPos.X / 2 - range; posX--) {
+		for (posY = curPos.Y; posY >= 0 && posY > curPos.Y - range; posY--) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		for (posY = curPos.Y; posY < STAGE_Y && posY < curPos.Y + range; posY++) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		if (STAGE[curPos.Y][posX] == WALL) break;
+	}
+	for (posX = curPos.X / 2; posX < STAGE_X / 2 && posX < curPos.X / 2 + range; posX++) {
+		for (posY = curPos.Y; posY >= 0 && posY > curPos.Y - range; posY--) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		for (posY = curPos.Y; posY < STAGE_Y && posY < curPos.Y + range; posY++) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		if (STAGE[curPos.Y][posX] == WALL) break;
+	}
+	for (posY = curPos.Y; posY >= 0 && posY > curPos.Y - range; posY--) {
+		for (posX = curPos.X / 2; posX >= 0 && posX > curPos.X / 2 - range; posX--) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		for (posX = curPos.X / 2; posX < STAGE_X / 2 && posX < curPos.X / 2 + range; posX++) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		if (STAGE[posY][curPos.X / 2] == WALL) break;
+	}
+	for (posY = curPos.Y; posY < STAGE_Y && posY < curPos.Y + range; posY++) {
+		for (posX = curPos.X / 2; posX >= 0 && posX > curPos.X / 2 - range; posX--) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		for (posX = curPos.X / 2; posX < STAGE_X / 2 && posX < curPos.X / 2 + range; posX++) {
+			if (posX == curPos.X / 2 && posY == curPos.Y) continue;
+			showStage(STAGE[posY][posX], posX * 2, posY);
+			if (STAGE[posY][posX] == WALL) break;
+		}
+		if (STAGE[posY][curPos.X / 2] == WALL) break;
+	}
 }
