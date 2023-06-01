c82fcdca0bc954cc7ccbec3e3039aaa604ec0339
unive1017 <tjgh219@gmail.com>
2023-06-01 23:56:04 +0900

    main함수 업데이트 아이템 획득 기능 추가 스크립트 시스템 추가 플레이어 사망 시스템 추가 다음 스테이지 전환 기능 추가

--- main.c
+++ main.c
@@ -3,26 +3,39 @@
 #include "interface.h"
 #include "stage.h"
 #include "Player.h"
+#include "ItemAndWeapon.h"
+#include "script.h"
 #include <conio.h>
 #include <time.h>
 
 int main(void)
 {
-    system("mode con cols=180 lines=50");
-    RemoveCursor();
+	system("mode con cols=130 lines=42");
+	RemoveCursor();
 
-    // 스테이지 출력
-    StageInforInit(1);
-    //printStage();
+	// 스테이지 출력
+	StageInforInit(1);
+	//printStage();
 
-    SetCurrentCursorPos(player.playerPos.X, player.playerPos.Y);
-    ShowPlayer();
-
-    while (1)
-    {
-        ProcessKeyInput();
-        PrintUI();
-    }
-    getchar();
-    return 0;
+	ShowPlayer();
+	StageInforInit(1);
+	getScript(0);
+	printScriptQueue();
+	ShowPlayer();
+	getScript(1);
+	printScriptQueue();
+	clearStage();
+	while (1) {
+		ShowPlayer();
+		if (isPlayerDead() == 1) {
+			StageInforInit(stageNum);
+		}
+		if (gun.shotOrNot && !showBullet()) {
+			continue;
+		}
+		ProcessKeyInput();
+		PrintUI();
+	}
+	getchar();
+	return 0;
 }
