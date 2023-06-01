1fa3ce227798cbd800a451fff8580eca8a35a80e
unive1017 <tjgh219@gmail.com>
2023-06-02 02:43:02 +0900

    몬스터 관련 사운드 추가

--- interface.h
+++ interface.h
@@ -36,7 +36,7 @@
 #define SCRIPT_POS_X STAGE_POS_X + (STAGE_X - SCRIPT_X) / 2
 #define SCRIPT_POS_Y STAGE_POS_Y + (STAGE_Y - SCRIPT_Y) / 2
 
-#define MAX_LIFE 3
+#define MAX_LIFE 3			// 라이프 최대 보유 가능 수
 #define INIT_WEAPON 0	// 처음 시작할 때 보유할 탄피 수
 #define MAX_ROCK_SHOWRANGE 5
 
@@ -48,12 +48,13 @@
 #define GET_ITEM2 2		// 포션 획득
 #define GET_GUN 3		// 탄피 획득
 #define GET_KEY 4		// 열쇠 획득
-#define USE_ITEM1 5		// 돌멩이 사용
-#define	USE_ITEM2 6		// 포션 사용
-#define USE_GUN1 7		// 탄피 사용(탄피 미보유 시)
-#define USE_GUN2 8		// 탄피 사용(탄피 보유 시)
-#define USE_KEY 9
-#define DAMAGE 10		// 플레이어가 데이지 입음
-#define GAMEOVER 11		// 게임오버 효과음
-#define MAIN 12			// 메인화면 배경음
-#define SELECT_MANU 13  // 메인화면 메뉴 효과음
+#define MONSTER 5		// 몬스터 소리
+#define USE_ITEM1 6		// 돌멩이 사용
+#define	USE_ITEM2 7		// 포션 사용
+#define USE_GUN1 8		// 탄피 사용(탄피 미보유 시)
+#define USE_GUN2 9		// 탄피 사용(탄피 보유 시)
+#define USE_KEY 10
+#define DAMAGE 11		// 플레이어가 데이지 입음
+#define GAMEOVER 12		// 게임오버 효과음
+#define MAIN 13			// 메인화면 배경음
+#define SELECT_MANU 14  // 메인화면 메뉴 효과음
